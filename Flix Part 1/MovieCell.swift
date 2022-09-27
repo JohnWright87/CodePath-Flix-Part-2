db7924c7ae728f41093a0f5e297751cec12e890b
JohnWright87 <85178608+JohnWright87@users.noreply.github.com>
2022-09-26 23:09:20 -0400

    Added CodePath Flix Part 2

---
+++ Flix Part 1/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  Flix Part 1
+//
+//  Created by John Wright on 9/23/22.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Flix Part 1/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix Part 1
+//
+//  Created by John Wright on 9/26/22.
+//
+
+import UIKit
+
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
